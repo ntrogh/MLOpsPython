1650bb1bfc762929361b92340456e29d08f58da3
Eugene Fedorenko <efedorenko@J32Z8H2.redmond.corp.microsoft.com>
2019-10-11 12:14:05 -0700

    test db util

--- code/training/R/train_with_r_on_databricks.py
+++ code/training/R/train_with_r_on_databricks.py
@@ -1,18 +1,17 @@
 import os
 import argparse
 import sys
-sys.path.append(os.path.abspath("."))  # NOQA: E402
-from test_db_util import test_print
-
 parser = argparse.ArgumentParser("train")
 parser.add_argument(
     "--AZUREML_SCRIPT_DIRECTORY_NAME",
     type=str,
     help="folder",
 )
-
 args, unknown = parser.parse_known_args()
 folder = args.AZUREML_SCRIPT_DIRECTORY_NAME
+sys.path.append(os.path.abspath("/dbfs/" + folder"))  # NOQA: E402
+from test_db_util import test_print
+
 
 os.system("cd " + "/dbfs/" + folder +
           " && Rscript r_train.r && ls -ltr model.rds")
